dfe7b3f49a02319be3dbacb1f7aa0798729b48b2
Jan Mad <jan.madon@gmail.com>
2023-08-24 17:27:47 +0200

    L52 - automatyczne escepowanie danych w Blade.php

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,30 +3,26 @@
 @section('title', 'Użytkownik')
 
 @section('content')
-    <h1>
-        users list
-    </h1>
 
-    <div>
-        wspołdzielenie zmiennych: {{$appName}}
-    </div>
     <hr>
-    <div>
-        user id: {{$id}}
-    </div>
+    <h3>Informacje o urzytkowniku:</h3>
+    <ul>
+        <li>Id: {{$user['id']}}</li>
+        <li>Imię: {{$user['firstName']}}</li>
+        <li>Nazwisko: {{$user['lastName']}}</li>
+        <li>Miasto: {{$user['city']}}</li>
+    </ul>
+{{-- DOMYŚLNIE UZYWAJ TEJ KONSTRUKCJI
+    ten html się nie wyręderuje: dak jest ustawione deufoltowo (to jest zapiezpiecznie takie)
+    wszystkie tagi escepuje również kod JS
+    <b>Bold HTML</b> --}}
+    <div>{{$user['html']}}</div>
+
+    {{-- tak się wyświetla bez escepowania:
+        Bold HTML --}}
+    {!! $user['html'] !!}
     <hr>
-    <div>
-        example: <?php echo $example?> to samo co wyzej
-    </div>
+
 @endsection
-
-@section('siderbar')
-   Rodzic @parent
-
-
-   Dziecko<li>siderbar z dziecka</li>
-@endsection
-
-@show
 
 
