968e2a31838cd9a4736e74af6eed60d7aecd7bd8
Jan Mad <jan.madon@gmail.com>
2023-09-02 07:13:50 +0200

    przelaczenie sesji z pliku do bazy danych

---
+++ database/migrations/2023_09_02_050551_create_sessions_table.php
@@ -0,0 +1,32 @@
+<?php
+// stworzenie: php artisan session:table
+// migracja tworzy tabele sesyjną robimy to wtedy gdy sesie(session driver) przelaczmy na database
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sessions');
+    }
+};
