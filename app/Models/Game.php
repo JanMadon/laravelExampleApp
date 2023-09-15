037f4fe9b20b527381b98ca83de86895accfd69c
Jan Mad <jan.madon@gmail.com>
2023-09-16 00:37:35 +0200

    klasa modelu / ustawienia w modelu

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -1,5 +1,6 @@
 <?php
 // php artisan make:model Game
+// jeden model na jedną tabelę
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -7,5 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    use HasFactory;
+    use HasFactory; // <- tego w starszej wersji nie było sprawdz po co to jest
+    // protected $table = 'application_games';
+    // protected $table = 'games'; // defoult
+    // protected $primaryKey = 'email';
+    // protected $primaryKey = 'id'; // defoult
+    // protected $timestamps = false; // gdy niema kolumny created_at i updated_at
+
+    protected $attributes = [
+        // domyśle waryości dla konkretnych kolumn np:
+        'score' => 5
+    ];
 }
