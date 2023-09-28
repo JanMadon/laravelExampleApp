fbccfe94b12cb2336c600b3e819e75f96f600e5a
Jan Mad <jan.madon@gmail.com>
2023-09-28 16:57:10 +0200

    genrenowanie URL url(), route(), action()

--- app/Http/Controllers/Home/MainPage.php
+++ app/Http/Controllers/Home/MainPage.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Game\GameController;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -22,6 +23,21 @@ class MainPage extends Controller {
         //dd($id);
         //Auth::logout(); wyloguje
 
+        $gameId = 44;
+        $url = url("gemes/$gameId");
+        $url = url()->current();// aktulany url ale bez query parameters
+        //$url = url()->full(); //pełna scieżka z parametrami
+        $url = url()->previous(); //url poprzedniej strony
+
+        $routUrl = route('games.show', ['gameId' => $gameId, // to jest parametr wymagany (zawartu w sciezce)
+                                            'foo' => 'bar']); // a to są gwury paramiters po ?
+
+        $actionUrl = action([GameController::class, 'dashboard'], ['foo'=>'bar']); // ganeruje url z danygo kontrolera, i nazwy metody 
+
+        // dump($actionUrl);
+        // dump($routUrl);
+        // dump($url);
+        // dd('exit');
         return view('home.main', ['user' => $user]);
     }
 }
